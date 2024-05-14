2c8a5dd978b88081545f9f53f89aecd7d4c7f392
Петров Александр <petrov1c@yandex.ru>
2024-05-14 14:30:01 +0300

    Настроил более правильный прунинг голов

--- src/pruning_module.py
+++ src/pruning_module.py
@@ -4,7 +4,7 @@ import torch
 from transformers.models.bert.modeling_bert import BertSelfAttention
 
 from src.datamodule import SentenceDM
-from src.lightning_module import PruneModule
+from src.lightning_module import PruneModule, chain
 
 from copy import deepcopy
 from tqdm import tqdm
@@ -13,18 +13,14 @@ from tqdm import tqdm
 def prune_model(lightning_module: PruneModule, datamodule: SentenceDM):
     device = 'cuda' if lightning_module.config.accelerator == 'gpu' else 'cpu'
 
-    if lightning_module.config.pruning.save_model:
-        lightning_module.teacher_model.to('cpu')
-        torch.save(lightning_module.teacher_model, 'models/origin_model.pth')
-
     model = deepcopy(lightning_module.teacher_model)
     model.to(device)
 
-    input_example = torch.randint(
-        high=model.embeddings.word_embeddings.num_embeddings,
-        size=(datamodule.cfg.batch_size, lightning_module.max_length),
-        device=device,
-    )
+    datamodule.setup('fit')
+    train_dataloader = datamodule.train_dataloader()
+
+    batch = next(iter(train_dataloader))
+    input_example = lightning_module.tokenize([text for text in chain(*batch)], device)
 
     ops, params = tp.utils.count_ops_and_params(model, input_example)
     logging.info(f'Model complexity: {ops / 1e6} MMAC, {params / 1e6} M params')
@@ -34,6 +30,13 @@ def prune_model(lightning_module: PruneModule, datamodule: SentenceDM):
         if name in ('pooler', 'embeddings'):
             ignored_layers.append(module)
 
+    num_heads = {}
+    for m in model.modules():
+        if isinstance(m, BertSelfAttention):
+            num_heads[m.query] = m.num_attention_heads
+            num_heads[m.key] = m.num_attention_heads
+            num_heads[m.value] = m.num_attention_heads
+
     taylor_criteria = tp.importance.GroupTaylorImportance()
     pruner = tp.pruner.MetaPruner(
         model,
@@ -42,11 +45,12 @@ def prune_model(lightning_module: PruneModule, datamodule: SentenceDM):
         pruning_ratio=lightning_module.config.pruning.pruning_ratio,
         global_pruning=lightning_module.config.pruning.global_pruning,
         iterative_steps=lightning_module.config.pruning.iterative_steps,
+        num_heads=num_heads,
+        prune_head_dims=True,
+        prune_num_heads=True,
+        head_pruning_ratio=lightning_module.config.pruning.pruning_ratio,
         ignored_layers=ignored_layers,
     )
-
-    datamodule.setup('fit')
-    train_dataloader = datamodule.train_dataloader()
 
     mse_loss = torch.nn.MSELoss()
     model.zero_grad()
@@ -71,8 +75,9 @@ def prune_model(lightning_module: PruneModule, datamodule: SentenceDM):
 
     for module in model.modules():
         if isinstance(module, BertSelfAttention):
-            module.attention_head_size = module.attention_head_size // 4
-            module.all_head_size = module.all_head_size // 4
+            module.num_attention_heads = pruner.num_heads[module.query]
+            module.attention_head_size = module.query.out_features // module.num_attention_heads
+            module.all_head_size = module.query.out_features
 
     lightning_module.student_model = model
 
@@ -80,5 +85,6 @@ def prune_model(lightning_module: PruneModule, datamodule: SentenceDM):
     logging.info(f'Model complexity (After taylor pruning): {ops / 1e6} MMAC, {params / 1e6} M params')
 
     if lightning_module.config.pruning.save_model:
+        model.zero_grad()
         model.to('cpu')
         torch.save(model, 'models/prune_model.pth')
