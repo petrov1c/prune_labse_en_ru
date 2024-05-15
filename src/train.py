31768d3250ddca0e4cc1839057b2a5711a3d0a34
Петров Александр <petrov1c@yandex.ru>
2024-05-15 19:31:10 +0300

    Теперь в clear.ml будет попадать процесс прунинга

--- src/train.py
+++ src/train.py
@@ -23,17 +23,16 @@ def arg_parse():
 def train(config: Config):
     os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
-    datamodule = SentenceDM(config.data_config)
-    model = PruneModule(config)
-
-    prune_model(model, datamodule)
-
     task = Task.init(
         project_name=config.project_name,
         task_name=f'{config.experiment_name}',
         auto_connect_frameworks=True,
     )
     task.connect(config.model_dump())
+
+    datamodule = SentenceDM(config.data_config)
+    model = PruneModule(config)
+    prune_model(model, datamodule)
 
     experiment_save_path = os.path.join(EXPERIMENTS_PATH, config.experiment_name)
     os.makedirs(experiment_save_path, exist_ok=True)
@@ -42,14 +41,15 @@ def train(config: Config):
         experiment_save_path,
         monitor=config.monitor_metric,
         mode=config.monitor_mode,
+        save_weights_only=True,
         save_top_k=1,
-        filename=f'epoch_{{epoch:02d}}-{{{config.monitor_metric}:.3f}}',
+        filename=f'epoch_{{epoch:02d}}-{{{config.monitor_metric}:.4f}}',
     )
     trainer = pl.Trainer(
         max_epochs=config.n_epochs,
         accelerator=config.accelerator,
         devices=[config.device],
-        log_every_n_steps=20,
+        log_every_n_steps=50,
         callbacks=[
             checkpoint_callback,
             EarlyStopping(monitor=config.monitor_metric, patience=4, mode=config.monitor_mode),
