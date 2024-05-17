232908db22eb4d560dbc9de6d8f2f813dca4ed3a
Петров Александр <petrov1c@yandex.ru>
2024-05-17 19:27:38 +0300

    Рефакторинг

--- src/lightning_module.py
+++ src/lightning_module.py
@@ -1,7 +1,5 @@
 import pytorch_lightning as pl
 import torch
-from torch import nn
-from torch.nn import functional as F
 
 from transformers import AutoTokenizer, AutoModel
 from itertools import chain
@@ -14,8 +12,8 @@ from src.metrics import get_metrics
 from src.utils import load_object
 from src.evalution import evaluate
 
-mse_loss = nn.MSELoss()
-kl_loss = nn.KLDivLoss()
+mse_loss = torch.nn.MSELoss()
+kl_loss = torch.nn.KLDivLoss()
 
 
 class PruneModule(pl.LightningModule):
@@ -25,7 +23,7 @@ class PruneModule(pl.LightningModule):
 
         self.tokenizer = AutoTokenizer.from_pretrained(self.config.model.name)
         self.teacher_model = AutoModel.from_pretrained(self.config.model.name)
-        self.student_model: Optional[nn.Module] = None
+        self.student_model: Optional[torch.nn.Module] = None
         self.max_length = self.teacher_model.embeddings.position_embeddings.num_embeddings
 
         self._losses = get_losses(self.config.losses)
@@ -37,9 +35,13 @@ class PruneModule(pl.LightningModule):
 
     def tokenize(self, input_text, device):
         return {
-            k: v.to(device) for k, v in
-            self.tokenizer(input_text, return_tensors='pt', padding=True, max_length=self.max_length,
-                           truncation=True).items()
+            k: v.to(device) for k, v in self.tokenizer(
+                input_text,
+                return_tensors='pt',
+                padding=True,
+                max_length=self.max_length,
+                truncation=True,
+            ).items()
         }
 
     def configure_optimizers(self):
@@ -70,8 +72,7 @@ class PruneModule(pl.LightningModule):
         inputs = self.tokenize([text for text in chain(*batch)], self.device)
         student_output = self.student_model(**inputs, output_attentions=True, output_hidden_states=True)
 
-        loss = self._calculate_loss(student_output, teacher_output, 'train_', len(batch[0]))
-        return loss
+        return self._calculate_loss(student_output, teacher_output, 'train_', len(batch[0]))
 
     def validation_step(self, batch, batch_idx):
         input_text = [text for text in chain(*batch)]
@@ -110,7 +111,6 @@ class PruneModule(pl.LightningModule):
         metrics = self._test_metrics.compute()
         self._benchmark_metrics_compute(metrics, 'test_')
 
-        # Сохраню модель костыль
         model = deepcopy(self.student_model)
         model.zero_grad()
         model.to('cpu')
@@ -119,7 +119,9 @@ class PruneModule(pl.LightningModule):
 
     def _benchmark_metrics_compute(self, metrics, prefix: str = ''):
         scores = evaluate(self.student_model, self.tokenizer)
-        metrics[prefix + 'mean_s'] = torch.tensor(scores['mean_s'].astype('float32'), device=self.device)
+        score = torch.tensor(scores['mean_s'].astype('float32'), device=self.device)
+        metrics['{0}mean_s'.format(prefix)] = score
+
         self.log_dict(metrics, on_epoch=True)
 
     def _calculate_loss(
@@ -130,6 +132,7 @@ class PruneModule(pl.LightningModule):
         batch_size: int,
     ) -> torch.Tensor:
 
+        # TODO вынести эти параметры в конфиг
         loss_weight = 0.5
         last_layer_loss_weight = 0.5
         intermediate_attn_layers_weights = (0, 0, 0, 1)
@@ -148,19 +151,17 @@ class PruneModule(pl.LightningModule):
         )
         self.log(f'{prefix}last_layer_loss', last_layer_loss.item(), batch_size=batch_size)
 
-        student_attentions, teacher_attentions = student_outputs.attentions, teacher_outputs.attentions
-        student_hidden_states, teacher_hidden_states = student_outputs.hidden_states, teacher_outputs.hidden_states
-
+        # TODO настроить стягивание промежуточных слоев
         intermediate_layer_att_loss = calc_intermediate_layers_attn_loss(
-            student_attentions,
-            teacher_attentions,
+            student_outputs.attentions,
+            teacher_outputs.attentions,
             intermediate_attn_layers_weights,
             student_teacher_attention_mapping,
         )
 
         intermediate_layer_feat_loss = calc_intermediate_layers_feat_loss(
-            student_hidden_states,
-            teacher_hidden_states,
+            student_outputs.hidden_states,
+            teacher_outputs.hidden_states,
             intermediate_feat_layers_weights,
         )
 
@@ -171,54 +172,17 @@ class PruneModule(pl.LightningModule):
         if intermediate_layer_feat_loss is not None:
             total_loss += intermediate_layer_feat_loss
 
-#                log_student_sm = F.log_softmax(student_outputs, dim=-1)
-#                teacher_sm = F.softmax(teacher_outputs, dim=-1)
-#                loss = cur_loss.loss(input=log_student_sm, target=teacher_sm)
-
         self.log(f'{prefix}total_loss', total_loss.item(), batch_size=batch_size)
         return total_loss
 
-    def _calculate_loss1(
-        self,
-        student_outputs: torch.Tensor,
-        teacher_outputs: torch.Tensor,
-        prefix: str,
-        batch_size: int
-    ) -> torch.Tensor:
-        total_loss = 0
-        for cur_loss in self._losses:
-            # Костыль (временный)
-            if cur_loss.name == 'kl':
-                log_student_sm = F.log_softmax(student_outputs, dim=-1)
-                teacher_sm = F.softmax(teacher_outputs, dim=-1)
-                loss = cur_loss.loss(input=log_student_sm, target=teacher_sm)
-            else:
-                loss = cur_loss.loss(student_outputs, teacher_outputs)
-            total_loss += cur_loss.weight * loss
-            self.log(f'{prefix}{cur_loss.name}_loss', loss.item(), batch_size=batch_size)
-        self.log(f'{prefix}total_loss', total_loss.item(), batch_size=batch_size)
-        return total_loss
 
-    # Костыль (временный)
-    #def calc_last_layer_loss(student_logits, teacher_logits, temperature, weight):
-    #    inputs = F.log_softmax(student_logits/temperature, dim=-1)
-    #    target = F.softmax(teacher_logits/temperature, dim=-1)
-    #    loss = nn.KLDivLoss(input=inputs, target=target) * temperature ** 2
-
-    #    return loss
-
-    # the whole batch, all tokens after the [cls], the whole dimension
-    # kd_loss = torch.nn.KLDivLoss(reduction='batchmean')(
-    #    F.log_softmax(outputs.prediction_logits[:, 1:, :] / temperature, dim=1),
-    #    F.softmax(big_out.prediction_logits[:, 1:, vocab_mapping] / temperature, dim=1)
-    #) / outputs.prediction_logits.shape[-1]
+def calc_last_layer_loss(student_logit, teacher_logit):
+    return mse_loss(student_logit, teacher_logit)
 
 
-def calc_last_layer_loss(student_logits, teacher_logits):
-    return mse_loss(student_logits, teacher_logits)
-
-def calc_intermediate_layers_attn_loss(student_logits, teacher_logits, weights, student_teacher_attention_mapping):
+def calc_intermediate_layers_attn_loss(student_logit, teacher_logit, weights, student_teacher_attention_mapping):
     return None
+
 
 def calc_intermediate_layers_feat_loss(student_feat, teacher_feat, weights):
     return None
