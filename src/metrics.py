3f131dfd19b452718b1c4d6c6b0216dc7766e437
Петров Александр <petrov1c@yandex.ru>
2024-05-17 16:42:22 +0300

    Добавил метрику для бенчмарка

--- src/metrics.py
+++ src/metrics.py
@@ -1,9 +1,24 @@
+import torch
 from torchmetrics import MeanSquaredError, MetricCollection
+from torchmetrics import Metric
+
+
+class BenchmarkAccuracy(Metric):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_state("mean_s", default=torch.tensor(0))
+
+    def update(self, *args, **kwargs) -> None:
+        pass
+
+    def compute(self) -> torch.Tensor:
+        return self.mean_s
 
 
 def get_metrics(**kwargs) -> MetricCollection:
     return MetricCollection(
         {
             'mse': MeanSquaredError(**kwargs),
+            'mean_s': BenchmarkAccuracy(**kwargs),
         }
     )
